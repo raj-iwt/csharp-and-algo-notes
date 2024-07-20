6fcde873f56c1f458dd104bb5a96102b8c778c15
Rajkumar Palaniyappan <rpalaniyappan@adventintermodal.com>
2024-07-19 21:58:27 -0700

    Added more problems in array

--- sorting/08_first_occurence_binary_search.py
+++ sorting/08_first_occurence_binary_search.py
@@ -1,17 +1,17 @@
 def find_occurence(arr, target):
     left, right = 0, len(arr) - 1
-    position = float('inf')
+    position = -1
     while (left <= right):
-        mid = left + right // 2
+        mid = (left + right) // 2
         if arr[mid] == target:
-            position = min(mid, position)
-            right = min(mid + 1, right - 1)
+            position = mid
+            right = mid - 1
         elif arr[mid] < target:
             left = mid + 1
         else:
-            right = mid + 1
+            right = mid - 1
 
-    return position if position != float('inf') else -1
+    return position
 
 
 arr = [2, 2, 3, 3, 3, 4, 5, 6, 8]
